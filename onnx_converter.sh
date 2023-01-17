3f19f8acc9046c8f81c37e633249fa3b0ae60c63
nachihebbar@gmail.com <nachihebbar@gmail.com>
2023-01-17 08:09:21 +0000

    Updated Onnx converter. Running python script to create spec.json

--- onnx_converter.sh
+++ onnx_converter.sh
@@ -15,27 +15,15 @@ echo "- ------------------------------------------"
 
 if [ "$model_framework" == "1" ]
 then
-	read -p "Enter your data.yaml path : " data_yaml
-	echo "- ------------------------------------------"
-	eval $(parse_yaml $data_yaml)
-else
-	read -p "Enter your model config file: " data_cfg
-	echo "- ------------------------------------------"
-	eval $(parse_yaml $data_cfg)
-fi
-read -p "Enter your model_config.yaml path : " config_yaml
-echo "- ------------------------------------------"
-echo "Anchors for your model is: $anchors"
-echo -e "Labels are $names"
-echo "$names" > names.txt
-echo "The number of classes in your model is: $nc"
-read -p "Did you train your model with default anchors?(y/n) : " default_anchors
-echo "-------------------------------------------"
+	read -p "Good choice with selecting Pytorch framework! Enter your data.yaml file path(contains class names,etc): " data_yaml
+	echo "--------------------------------------------------------------------------------"
+fi 
+
 read -p "What is the image size you trained your model on?(Enter just the width or height) : " img_size
 echo "-------------------------------------------"
 cat model_folder_names.txt
 echo "-------------------------------------------"
-read -p "Enter a folder name where we'll export your model to. Follow the naming convention <object_class>.<model_type> : " folder_name
+read -p "Enter a folder name where we'll export your model. Follow the above naming convention : " folder_name
 
 if [ "$model_type" == "3" ]
 then 
@@ -57,18 +45,36 @@ then
 	python export.py --weights "../"$weights --grid --include-nms --dynamic-batch --img-size $img_size
 	onnx_model_name="${weights:0:-3}"".onnx"
 	mv "../"$onnx_model_name "../"$folder_name/onnx/v1/
-	cp "../boilerplate_json/yolov7_spec.json" "../"$folder_name/onnx/v1/
+	python ../yaml_reader.py $model_type $img_size $folder_name $onnx_model_name $data_yaml
+	#$cp "../boilerplate_json/yolov7_spec.json" "../"$folder_name/onnx/v1/
 	cd "../"$folder_name/onnx/v1
-	echo "Done! The following folder has been created for you: $folder_name. Kindly edit the labels and model path in you spec.json as per your project"
+	echo "Done! The following folder has been created for you: $folder_name. You can transfer this awiros alexandria folder and start using it!"
 
 elif [ "$model_type" == "2" ]
 then
 	echo "v5 here we come"
+	mkdir $folder_name && mkdir $folder_name/onnx && mkdir $folder_name/onnx/v1
 	cd yolov5
 	python export.py --weights "../"$weights --include 'onnx' --dynamic --img-size $img_size
+	onnx_model_name="${weights:0:-3}"".onnx"
+	mv "../"$onnx_model_name "../"$folder_name/onnx/v1/
+	#cp "../boilerplate_json/yolov5_spec.json" "../"$folder_name/onnx/v1/
+	python ../yaml_reader.py $model_type $img_size $folder_name $onnx_model_name $data_yaml
+	#cd "../"$folder_name/onnx/v1
+	#temp=$(mktemp)
+	#jq '. input_data.layers."shape"='"[-1,3,$img_size,$img_size]"'' yolov5_spec.json > "$temp" &&mv "$temp" yolov5_spec.json
+	#jq '. "onnx_path"="'"$folder_name/onnx/v1/$onnx_model_name"'"' yolov5_spec.json  > "$temp" &&mv "$temp" yolov5_spec.json
+	echo "Done! The following folder has been created for you: $folder_name. You can transfer this awiros alexandria folder and start using it!"
 	
 else
 	echo "v3 here we come"
+	mkdir $folder_name && mkdir $folder_name/onnx && mkdir $folder_name/onnx/v1
 	cd yolov3
 	python yolo_to_onnx.py -m "../"$weights 
+	onnx_model_name="$weights"".onnx"
+	mv "../"$onnx_model_name "../"$folder_name/onnx/v1/
+	python ../yaml_reader.py $model_type $img_size $folder_name $onnx_model_name $onnx_model_name 
+	#cp "../boilerplate_json/yolov3_spec.json" "../"$folder_name/onnx/v1/
+	#cd "../"$folder_name/onnx/v1
+	echo "Done! The following folder has been created for you: $folder_name."
 fi
